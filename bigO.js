27c9f102eacf53ecb2cb6fbaa4781b378c06b8d4
Zach Wood <zacharydavwood@gmail.com>
2025-04-22 13:19:16 -0400

    added big O comments

--- bigO.js
+++ bigO.js
@@ -3,4 +3,25 @@
     Big o notation 
 
 
+    - it shows us the performace of agorithms 
+    
+
+    Big O rules
+
+    - O(1) - constant time
+    - O(log n) - logarithmic time
+    - O(n) - linear time
+    - O(n^2) - quadratic time
+    - O(2^n) - exponential time
+
+
+    Let say we have (4n) it would be O(n)
+
+    we can drop any constant factors like numbers
+
+    
+
+
+
+
 */
