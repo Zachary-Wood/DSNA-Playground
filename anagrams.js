e604e18d5ee83ef255157ec11ae7faf91da8de92
Zach Wood <zacharydavwood@gmail.com>
2025-05-06 16:21:38 -0400

    added comments to file

--- anagrams.js
+++ anagrams.js
@@ -10,23 +10,23 @@
 const anagrams = (s1, s2) => {
 
 
-    const data = {}
+    const data = {} // hash map to store the letters and their counts
  
-    for (let item of s1) {
-      if (!(item in data)) data[item] = 0;
-      data[item] += 1
+    for (let item of s1) { // iterate through the first string and we add the letters to the hash map
+      if (!(item in data)) data[item] = 0; // if its not there we add it 
+      data[item] += 1 // then we increment the count 
     }
  
-   for (let item of s2) {
-     if (data[item] == undefined) return false
-     else data[item] -= 1
+   for (let item of s2) { // we then iterate through the second string 
+     if (data[item] == undefined) return false // if the letter is not in the hash map we return false
+     else data[item] -= 1 // if it is we decrement the count
    }
  
-   for (let item in data) {
-     if (data[item] != 0) return false
+   for (let item in data) { // we then iterate through the hash and see if any of the letter are not 0
+     if (data[item] != 0) return false // if they are not we return false
    }
  
-   return true
+   return true // if all of them are 0 we return true
  };
     
     
