65ab25f21a8d9e31177062ccf0c8589eb64efa0e
Zach Wood <zacharydavwood@gmail.com>
2025-05-07 16:20:32 -0400

    added intersection

---
+++ intersection.js
@@ -0,0 +1,21 @@
+const intersection = (a, b) => {
+    // todo
+  
+    const hash = {}
+    let result = []
+  
+    for (let item of a) {
+      if (!(item in hash)) hash[item] = 0
+      hash[item] += 1
+    }
+  
+    for (let item of b) {
+      if (item in hash) hash[item] += 1
+    }
+  
+    for (let res in hash) {
+      if (hash[res] == 2) result.push(Number(res))
+    }
+  
+    return result
+  };
