a46a52fcf1e2c0e5836c8892c0a37b77798cc2d4
Zach Wood <zacharydavwood@gmail.com>
2025-05-07 15:48:56 -0400

    added comments

--- pair.js
+++ pair.js
@@ -1,13 +1,13 @@
 const pairSum = (numbers, targetSum) => {
     // todo
-    let hash = {}
+    let hash = {} // we create a hash
   
-    for (let i = 0; i < numbers.length; i++) {
-      let num = numbers[i];
-      let comp = targetSum - num;
-      if (comp in hash) return [i, hash[comp]]
+    for (let i = 0; i < numbers.length; i++) { // we iterate through our numbers
+      let num = numbers[i]; // we set our num to be the number at the index
+      let comp = targetSum - num; // we set our comp to be the target sum minus the number at the index
+      if (comp in hash) return [i, hash[comp]] // if the comp is in the hash we return the index and the hash[comp]
   
-      hash[num] = i
+      hash[num] = i // if the number was not in the hash we set the num to be its index such as 3:0 and 4:1
     
       
     }
