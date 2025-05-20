3ed151d5735d82e4a1f59a75eacf7fd96e639548
Zach Wood <zacharydavwood@gmail.com>
2025-05-20 15:25:12 -0400

    added comments for value

--- linked-lists/LLvalue.js
+++ linked-lists/LLvalue.js
@@ -1,10 +1,10 @@
 const linkedListValues = (head) => {
     // todo
-    let res = []
-    let curr = head
+    let res = [] // array to return for final result 
+    let curr = head // set our curr variable to the head of the linked list
   
-    while(curr !== null) {
-      res.push(curr.val)
+    while(curr !== null) { // while next is not null
+      res.push(curr.val) // we push the current value and then we set the curr variable to the next node
       curr = curr.next
     }
     return res
